f0effb067a4368fb8768fe4d71c09ce889ff8c6d
Johnny <52709518+1starexpert@users.noreply.github.com>
2026-02-11 15:42:48 -0500

    Update clan_backend.py

--- clash_of_clans/clan_backend.py
+++ clash_of_clans/clan_backend.py
@@ -4,11 +4,33 @@
 import requests
 
 from pprint import pprint
+import streamlit as st
 
 #API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI3ODI2ZGM5LWM3M2EtNGZmOS04NzEzLTQ0Nzc0M2ZhZjBmMyIsImlhdCI6MTc2NDYwNTAwNSwic3ViIjoiZGV2ZWxvcGVyLzViMjBmYzM1LTEwOGEtNDM1YS0yNmMwLTQ4Njk2OTA4MDQyOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0OS4xMDIuMjQyLjkxIl0sInR5cGUiOiJjbGllbnQifV19.OcbKnvwVw7lbPmkPMsl0kHS1Z88_mN3rstpAfm0LURK8DU38AihsPXjkhpsZLoxAN7VkhHaD_rKlvSpNnfIeGw"
 #API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdkYzEwOTliLTIwN2EtNDNkOC05OTMzLWQzMmJiYzllYWFiMiIsImlhdCI6MTc2NDYxNzcxOSwic3ViIjoiZGV2ZWxvcGVyLzViMjBmYzM1LTEwOGEtNDM1YS0yNmMwLTQ4Njk2OTA4MDQyOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0OS4xMDIuMjQyLjkwIl0sInR5cGUiOiJjbGllbnQifV19.LG-PxSC9JYBUMNK1NGNaHHd2Re9N_862sWSF_cB4Q8jZzpyD9cp2WLtJJrhPNOt9tgcRveysAUC-qYJm_i0KMA"
 API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjliYjE0MDVjLTMxY2MtNGY2MC1hOWY5LWViY2E3ODk2NzBhMyIsImlhdCI6MTc2NDYxODI3OSwic3ViIjoiZGV2ZWxvcGVyLzViMjBmYzM1LTEwOGEtNDM1YS0yNmMwLTQ4Njk2OTA4MDQyOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.C6EoL9aIDnsZ4ZYrPKKJvhoRkzrKan8HB6W7KMkFxyJHnjp1Ryj2Evo3J3DdfKb8hLiZWoaHILErNg-w1jg1tQ"
 
+def displayClanLeaders():
+    url = f"https://cocproxy.royaleapi.dev/v1/locations/32000009/rankings/clans?limit=10"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(url, headers=headers)
+    clan_dict = response.json()
+    #pprint(clan_dict["items"][0])
+    cd = clan_dict["items"]
+
+    for i in range(10):
+        lb = []
+        name = cd[i]["name"]
+        tag = cd[i]["tag"]
+        st.image(cd[i]["badgeURLs"]["small"])
+        st.write("Clan Name: " + name + " Clan Tag: " + tag)
+    #name = cd[0]["name"]
+    #tag = cd[0]["tag"]
+
+    #print(name)
+    #print(tag)
 def displayClanInformation(tag):
 
     CLAN_TAG = tag.replace("#", "%23")  # URL encode the hashtag
